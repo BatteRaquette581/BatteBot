8d7b5bc3f11f72d8957eb3c52c938dfe7fb52dff
BatteRaquette581 <84451047+BatteRaquette581@users.noreply.github.com>
2021-12-24 14:22:12 +0100

    Update and rename online (1).py to online.py

--- online.py
+++ online.py
@@ -1,23 +1,15 @@
 from flask import Flask
 from threading import Thread
 import random
-
-
-app = Flask('')
-
-@app.route('/')
+app = Flask("")
+@app.route("/")
 def home():
 	return 'Maintaining bot...'
-
 def run():
   app.run(
-		host='0.0.0.0',
+		host="0.0.0.0",
 		port=8080
 	)
-
 def keep_alive():
-	'''
-	Creates and starts new thread that runs the function run.
-	'''
 	t = Thread(target=run)
 	t.start()
